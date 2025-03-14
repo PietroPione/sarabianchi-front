adfa0da8a4208f4646cffded9b892c4c3ed76c36
PietroPione <pietrofrlavoro@gmail.com>
2025-03-14 18:18:56 +0100

    fix: scroll mobile

--- app/script/[slug]/page.jsx
+++ app/script/[slug]/page.jsx
@@ -1,17 +1,8 @@
 import PdfViewer from "@/components/PdfViewer";
 import { createClient } from "@/prismicio";
 
-export async function generateStaticParams() {
-  const client = createClient();
-  const response = await client.getByType("script");
-
-  return response.results.map((script) => ({
-    slug: script.uid,
-  }));
-}
-
 export default async function PdfPage({ params }) {
-  const { slug } = await params;
+  const { slug } = await params; // Aggiungi "await" qui
   const client = createClient();
 
   // Recupera il documento specifico in base allo slug
@@ -23,9 +14,8 @@ export default async function PdfPage({ params }) {
   }
 
   return (
-    <div className="container py-10" style={{ overflow: 'hidden' }}> {/* Blocca lo scorrimento della pagina */}
-      <div style={{ height: '80vh', overflowY: 'auto' }}> {/* Contenitore del PDF con scorrimento interno */}
-        {/* Visualizzazione del PDF */}
+    <div className="container py-10" style={{ overflow: 'auto' }}>
+      <div style={{ height: '80vh', overflow: 'visible' }}>
         {script.data.slices[1]?.primary?.script_pdf?.url && (
           <PdfViewer pdfUrl={script.data.slices[1].primary.script_pdf.url} />
         )}
@@ -33,7 +23,6 @@ export default async function PdfPage({ params }) {
       {script.data.slices.map((slice, index) => (
         <div key={index} className="mb-8 p-4 border border-gray-200 rounded-lg">
           <h2 className="text-xl font-bold mb-2">{slice.slice_type}</h2>
-          {/* Altri contenuti delle slice */}
         </div>
       ))}
     </div>
