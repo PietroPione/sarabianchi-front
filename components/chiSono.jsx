1bff5837725f18e4786d93aee265e17e6fa5e0a3
PietroPione <pietrofrlavoro@gmail.com>
2025-03-21 18:47:38 +0100

    fix: vari

--- components/chiSono.jsx
+++ components/chiSono.jsx
@@ -1,7 +1,4 @@
-import { isFilled, asImageSrc } from "@prismicio/client";
 import { PrismicRichText } from "@prismicio/react";
-import Image from "next/image"; // Importa il componente Image
-
 import { createClient } from "@/prismicio";
 
 export default async function ChiSono() {
@@ -10,40 +7,35 @@ export default async function ChiSono() {
 
   const bioSlice = page.data.slices.find((slice) => slice.slice_type === "bio");
 
-  const imageUrl = asImageSrc(bioSlice.primary.immagine_bio);
-
   return (
-    <div className="space-y-10">
-      <div >
+    <div className="space-y-20">
+      <div className="space-y-10 flex flex-col items-center">
         <h2 className="text-tertiary text-40 md:text-60 uppercase font-bold">
           {bioSlice.primary.titolo}
         </h2>
-        <div className="text-justify">
+        <div className="text-justify md:max-w-[66vw] lg:max-w-[50vw]">
           <PrismicRichText field={bioSlice.primary.testo_bio} />
         </div>
       </div>
-      <div className="flex flex-col md:flex-row space-y-10 md:space-y-0 justify-between">
-        <Image
-          src={bioSlice.primary.immagine_bio.url}
-          alt="Immagine Bio"
-          width={1000}
-          height={1000}
-          className="max-w-full md:max-w-[20vw] lg:max-w-[20vw] max-w-auto object-contain"
-        />
-        <Image
-          src={bioSlice.primary.immagine_bio_2.url}
-          alt="Immagine Bio"
-          width={1000}
-          height={1000}
-          className="max-w-full md:max-w-[20vw] lg:max-w-[20vw] max-w-auto object-contain"
-        />
-        <Image
-          src={bioSlice.primary.immagine_bio_3.url}
-          alt="Immagine Bio"
-          width={1000}
-          height={1000}
-          className="max-w-full md:max-w-[20vw] lg:max-w-[20vw] max-w-auto object-contain"
-        />
+      <div className="flex flex-col md:flex-row space-y-10 md:space-y-0  justify-between md:justify-center md:space-x-8">
+        {bioSlice.primary.immagine_bio.url && bioSlice.primary.immagine_bio.url.trim() !== "" && (
+          <div
+            className="md:w-[20vw] lg:w-[15vw] h-auto aspect-square bg-cover bg-center"
+            style={{ backgroundImage: `url(${bioSlice.primary.immagine_bio.url})` }}
+          ></div>
+        )}
+        {bioSlice.primary.immagine_bio_2.url && bioSlice.primary.immagine_bio_2.url.trim() !== "" && (
+          <div
+            className="md:w-[20vw] lg:w-[15vw] h-auto aspect-square bg-cover bg-center"
+            style={{ backgroundImage: `url(${bioSlice.primary.immagine_bio_2.url})` }}
+          ></div>
+        )}
+        {bioSlice.primary.immagine_bio_3.url && bioSlice.primary.immagine_bio_3.url.trim() !== "" && (
+          <div
+            className="md:w-[20vw] lg:w-[15vw] h-auto aspect-square bg-cover bg-center"
+            style={{ backgroundImage: `url(${bioSlice.primary.immagine_bio_3.url})` }}
+          ></div>
+        )}
       </div>
     </div>
   );
